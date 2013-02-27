0ef1311294fab2846fe3f53af161089afcf81914
Brian Brooks <brooks.brian@gmail.com>
2013-02-27 18:25:20 -0500

    server.hs

--- examples/server.hs
+++ examples/server.hs
@@ -6,6 +6,8 @@ import Network.Socket.ByteString
 import Network.OpenFlow
 import Network.Info
 import System.Environment
+import Data.Bits
+import GHC.Word
 
 main :: IO ()
 main = do
@@ -24,50 +26,87 @@ serveConns sock =
   forever $ do
     (csock, caddr) <- accept sock
     handshake csock
+    flood csock
     _ <- forkIO $ handleSwitch csock caddr
     return ()
-
-
-emptyMac = MAC 0x00 0x00 0x00 0x00 0x00 0x00
-
-inp  = 1
-outp = 6
-
-wc = [--OfpfwInPort,
-  OfpfwDlVlan, OfpfwDlSrc
-  , OfpfwDlDst, OfpfwDlType, OfpfwNwProto
-  , OfpfwTpSrc, OfpfwTpDst, OfpfwNwSrcShift
-  , OfpfwNwSrcBits, OfpfwNwSrcMask, OfpfwNwSrcAll
-  , OfpfwDlVlanPcp, OfpfwNwTos, OfpfwAll
-  ]
-m = OfpMatch wc inp emptyMac emptyMac 0 0 0 0 0 0 0 0 0
-
-act1 = OfpOutput 5 256
-act2 = OfpOutput 6 256
-uint32max = 0xffffffff
-pri = 1
-flowMod = OfpFlowMod m 0 OfpfcAdd 360 360 pri uint32max outp OfpffSendFlowRem [act1, act2]
-
 
 handshake :: Socket -> IO ()
 handshake sock = do
   sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 0) (OfptHello []))
   frame <- readOfpFrame sock
-  putStrLn $ "frame: " ++ show frame
-  sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 1) (OfptFeaturesRequest))
+  putStrLn $ "RX: " ++ show frame
+  sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 1) (OfptGetConfigRequest))
   frame <- readOfpFrame sock
-  putStrLn $ "frame: " ++ show frame
-  
-  sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 2) (OfptFlowMod flowMod))
-
+  putStrLn $ "RX: " ++ show frame  
+  sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 2) (OfptFeaturesRequest))
+  frame <- readOfpFrame sock
+  putStrLn $ "RX: " ++ show frame  
 
 handleSwitch :: Socket -> SockAddr -> IO ()  
 handleSwitch sock caddr =
   forever $ do
     frame <- readOfpFrame sock
-    putStrLn $ "frame: " ++ show frame
+    putStrLn $ "RX: " ++ show frame
     case frame of
       (OfpFrame (OfpHeader _ _ _ xid) (OfptEchoRequest dat)) -> do
-        sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 xid) (OfptEchoReply dat))
-        putStrLn "sent echo reply"
-      _ -> putStrLn "unhandled frame"
+        let resp = (OfpFrame (OfpHeader 1 0 0 xid) (OfptEchoReply dat))
+        sendAll sock $ encode resp
+        putStrLn "TX: " ++ show resp
+      (OfpFrame (OfpHeader _ _ _ xid) (OfptPacketIn (OfpPacketIn bid len inp reason dat))) ->
+        let resp = (OfpFrame (OfpHeader 1 0 0 xid) (OfptPacketOut (OfpPacketOut bid inp [(OfpOutput 6 0)] dat)))
+        sendAll sock $ encode resp
+        putStrLn "TX: " ++ show resp
+      _ -> putStrLn " UNHANDLED"
+
+
+inp  = 1
+outp = 6
+ofppController = 0xfffd
+ofppNone = 0xffff
+
+pri = 1
+uint32max = 0xffffffff
+emptyMac = MAC 0x00 0x00 0x00 0x00 0x00 0x00
+dlVlan' = 10
+
+wc = [
+  OfpfwInPort,
+  OfpfwDlVlan,
+  OfpfwDlSrc,
+  OfpfwDlDst,
+  OfpfwDlType,
+  OfpfwNwProto,
+  OfpfwTpSrc,
+  OfpfwTpDst,
+  OfpfwNwSrc,
+  OfpfwNwDst,
+  OfpfwDlVlanPcp,
+  OfpfwNwTos
+  ]
+
+
+incrMac :: MAC -> Word64 -> MAC
+incrMac (MAC a b c d e f) v =
+  MAC (fromIntegral $ (m `shiftR` 40) .&. 0xFF)
+      (fromIntegral $ (m `shiftR` 32) .&. 0xFF)
+      (fromIntegral $ (m `shiftR` 24) .&. 0xFF)
+      (fromIntegral $ (m `shiftR` 16) .&. 0xFF)
+      (fromIntegral $ (m `shiftR`  8) .&. 0xFF)
+      (fromIntegral $ m .&. 0xFF)
+  where
+    m = v + fromIntegral ((a `shiftL` 40) .|.
+                          (b `shiftL` 32) .|.
+                          (c `shiftL` 24) .|.
+                          (d `shiftL` 16) .|.
+                          (e `shiftL`  8) .|. f)
+
+flood :: Socket -> IO()
+flood sock = flood' 4 0
+  where
+    flood' 0 xid = putStrLn $ "done! xid=" ++ show xid
+    flood' n xid = do
+      let m  = OfpMatch wc inp emptyMac (incrMac emptyMac n) dlVlan' 0 0 0 0 0 0 0 0
+      let fm = OfpFlowMod m 0 OfpfcAdd 65535 65535 pri uint32max outp OfpffSendFlowRem [(OfpOutput outp 0)]
+      sendAll sock $ encode (OfpFrame (OfpHeader 1 0 0 xid) (OfptFlowMod fm))
+      putStrLn $ "TX: " ++ show n
+      flood' (n-1) (xid+1)
